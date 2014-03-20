fa35a0eb7ccb2f8aaca1f90306609ee1f1125c43
JamieTabone <jamie.tabone8@gmail.com>
2014-03-20 12:55:14 +0100

    Testing Framework DB and Database Class

---
+++ Bit8TestingFramework_v2.0/Bit8TestingFramework_v2.0/TestCaseModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit8TestingFramework_v2._0
+{
+    class TestCaseModel
+    {
+        public string testName;
+        public string description;
+        public Int32 caseNumber;
+        public DateTime dateTime;
+        public string osVersion;
+        public string platform;
+        public string machineName;
+        public string machineUser;
+        public int asserts;
+        public bool executed;
+        public string result;
+        public bool success;
+        public string message;
+        public string stackTrace;
+        public double runningTime;
+        public string codeCoverageType;
+        public float codeCoveragePercentage;
+
+        public TestCaseModel() 
+        {
+            this.testName = "";
+            this.description = "";
+            this.caseNumber = 0;
+            this.dateTime = DateTime.Now;
+            this.osVersion = "";
+            this.platform = "";
+            this.machineName = "";
+            this.machineUser = "";
+            this.asserts = 0;
+            this.executed = false;
+            this.result = "";
+            this.success = false;
+            this.message = "";
+            this.stackTrace = "";
+            this.runningTime = 0;
+            this.codeCoverageType = "";
+            this.codeCoveragePercentage = 0;
+        }
+    }
+}
---
+++ Bit8TestingFramework_v2.0/Bit8TestingFramework_v2.0/TestDatabase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Bit8TestingFramework_v2._0
+{
+    class TestDatabase
+    {
+        public static void SaveTest(TestCaseModel newTest) 
+        {
+
+        }
+
+        private MySqlConnection InitializeConnection() 
+        {
+            string server = "localhost";
+            string database = "testingframeworkdb";
+            string uid = "root";
+            string password = "jamie1234";
+            string connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection mysqlcon = new MySqlConnection(connectionString);
+            return mysqlcon;
+        }
+    }
+}

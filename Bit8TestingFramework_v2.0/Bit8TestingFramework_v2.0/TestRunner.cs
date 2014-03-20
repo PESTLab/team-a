753c2e351f777ad638dab5647d4966b91c0e5d09
JamieTabone <jamie.tabone8@gmail.com>
2014-03-20 11:04:43 +0100

    The Testing Framework Bit8 v2.0

---
+++ Bit8TestingFramework_v2.0/Bit8TestingFramework_v2.0/TestRunner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Mocks;
+using NUnit.Core;
+using NUnit.ConsoleRunner;
+using NUnit.Framework.Constraints;
+using System.IO;
+using NUnit.Util;
+
+namespace Bit8TestingFramework
+{
+    public class TestRunner
+    {
+        private String path;
+
+        public TestRunner(String path)
+        {
+            this.path = path;
+            TestResult tempTr = runTest();
+            XmlResultWriter xmlw = new XmlResultWriter(Directory.GetCurrentDirectory() + @"\BackupResults\testResult_" + this.nextFileName());
+            xmlw.SaveTestResult(tempTr);
+            //call class to xml parsing
+        }
+
+        private TestResult runTest()
+        {
+            CoreExtensions.Host.InitializeService();
+            TestPackage testPackage = new TestPackage(this.path);
+            RemoteTestRunner rtr = new RemoteTestRunner();
+            rtr.Load(testPackage);
+            TestResult tr = rtr.Run(new NullListener(), TestFilter.Empty, false, LoggingThreshold.Error);
+            XmlResultWriter xmlw = new XmlResultWriter(Directory.GetCurrentDirectory() + @"\TempResults\testResult.xml");
+            xmlw.SaveTestResult(tr);
+            return tr;
+        }
+
+        private string nextFileName()
+        {
+            string[] temp = Directory.GetFiles(Directory.GetCurrentDirectory() + @"\TempResults");
+            string last = temp[temp.Length - 1];
+            string bufferInteger = "";
+            for (int i = 0; i < last.Length; i++)
+            {
+                if (last[i] == '_')
+                {
+                    bufferInteger += last[i];
+                    continue;
+                }
+                else continue;
+            }
+            return bufferInteger;
+        }
+    }
+}
